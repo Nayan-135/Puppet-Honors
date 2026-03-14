e52b9c0058252574ab80e19e83542de694a84e3e
Nayan-135 <ghatenayan5@gmail.com>
2026-03-14 19:21:03 +0530

    third

--- site.pp
+++ site.pp
@@ -1,21 +1,33 @@
 node default {
 
-  # Level 1 - Create folder
+  # Level 1 - Folder
   file { 'C:/PuppetMission':
     ensure => directory,
   }
 
-  # Level 1 - Create hello file
+  # Level 1 - Hello file
   file { 'C:/PuppetMission/hello.txt':
     ensure  => file,
     content => "Puppet was here at Nayan Ghate",
     require => File['C:/PuppetMission'],
   }
 
-  # Level 2 - Stop and disable Print Spooler
+  # Level 2 - Stop Print Spooler
   service { 'Spooler':
     ensure => stopped,
     enable => false,
+  }
+
+  # Level 3 - Registry Key
+  registry_key { 'HKLM\Software\PuppetMaster':
+    ensure => present,
+  }
+
+  # Level 3 - Registry Value
+  registry_value { 'HKLM\Software\PuppetMaster\MissionStatus':
+    ensure => present,
+    type   => string,
+    data   => 'Success',
   }
 
 }

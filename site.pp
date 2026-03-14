8c77e028b59c8f1b1c4c7bf13ed2f97e6dc9934d
Nayan-135 <ghatenayan5@gmail.com>
2026-03-14 19:13:16 +0530

    secound

--- site.pp
+++ site.pp
@@ -1,13 +1,21 @@
 node default {
 
+  # Level 1 - Create folder
   file { 'C:/PuppetMission':
     ensure => directory,
   }
 
+  # Level 1 - Create hello file
   file { 'C:/PuppetMission/hello.txt':
     ensure  => file,
     content => "Puppet was here at Nayan Ghate",
     require => File['C:/PuppetMission'],
+  }
+
+  # Level 2 - Stop and disable Print Spooler
+  service { 'Spooler':
+    ensure => stopped,
+    enable => false,
   }
 
 }
